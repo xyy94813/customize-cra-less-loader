d31e3610d97ecb826eac44ad7cf983fc305f0c58
RoXoM <xyy94813@sina.com>
2020-08-19 10:07:02 +0800

    feat: add less loader plugin
    support with `css-loader` v3.

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # customize-cra-less-loader
-Add less loader to any create-react-app using customize-cra
+
+Add less loader to any `create-react-app` using `customize-cra`.
+Support with `css-loader` v3.
+
+> If [PR 255](https://github.com/arackaf/customize-cra/pull/255) been merged into master,
+> you don't need this modules
+
+## Usage
+
+First, install less and less-loader packages:
+
+```sh
+npm i -D less less-loader
+// or yarn add --dev less less-loader
+```
+
+After it's done, call addLessLoader in override like below:
+
+```js
+const { override } = require("customize-cra");
+const addLessLoader = require("customize-cra-less-loader");
+
+module.exports = override(addLessLoader(loaderOptions));
+```
+
+`loaderOptions` is optional. If you have Less specific options, you can pass to it. For example:
+
+```js
+const { override } = require("customize-cra");
+const addLessLoader = require("customize-cra-less-loader");
+
+module.exports = override(
+  addLessLoader({
+    cssLoaderOptions: {
+      onlyLocals: true,
+      modules: {
+        localIdentName: "[hash:base64:8]",
+      },
+    },
+    lessLoaderOptions: {
+      lessOptions: {
+        strictMath: true,
+      },
+    },
+  })
+);
+```
+
+Check Less document for all available specific options you can use.
+
+Once `less-loader` is enabled, you can import `.less` file in your project.
+
+And `.module.less` will use CSS Modules.
+
+> if you use TypeScript (npm init react-app my-app --typescript) with CSS Modules, you should edit react-app-env.d.ts.
+
+```ts
+declare module "*.module.less" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+```
+
+## Contribution
+
+DefinitelyTyped only works because of contributions by users like you!
+
+### Git Message
+
+[Follow the Angular git commit message specification](https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#commits)
+
+But, you can ignore the `scope`
+
+## Lisense
+
+MIT RoXoM <xyy94813@sina.com>
---
+++ src/index.js
@@ -0,0 +1,113 @@
+export default (loaderOptions = {}) => config => {
+  const MiniCssExtractPlugin = require.resolve('mini-css-extract-plugin');
+  const postcssNormalize = require.resolve('postcss-normalize');
+
+  const cssLoaderOptions = loaderOptions.cssLoaderOptions || {};
+  const lessLoaderOptions = loaderOptions.lessLoaderOptions || {};
+
+  const lessRegex = /\.less$/;
+  const lessModuleRegex = /\.module\.less$/;
+
+  const webpackEnv = process.env.NODE_ENV;
+  const isEnvDevelopment = webpackEnv === 'development';
+  const isEnvProduction = webpackEnv === 'production';
+  const shouldUseSourceMap = isEnvProduction
+    ? process.env.GENERATE_SOURCEMAP !== 'false'
+    : isEnvDevelopment;
+  const { publicPath } = config.output;
+  const shouldUseRelativeAssetPaths = publicPath.startsWith('.');
+
+  // copy from react-scripts
+  // https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/config/webpack.config.js#L93
+  const getStyleLoaders = (cssOptions, preProcessor) => {
+    const loaders = [
+      isEnvDevelopment && require.resolve('style-loader'),
+      isEnvProduction && {
+        loader: MiniCssExtractPlugin.loader,
+        // css is located in `static/css`, use '../../' to locate index.html folder
+        // in production `publicPath` can be a relative path
+        options: shouldUseRelativeAssetPaths ? { publicPath: '../../' } : {},
+      },
+      {
+        loader: require.resolve('css-loader'),
+        options: cssOptions,
+      },
+      {
+        loader: require.resolve('postcss-loader'),
+        options: {
+          ident: 'postcss',
+          plugins: () => [
+            require.resolve('postcss-flexbugs-fixes'),
+            require.resolve('postcss-preset-env')({
+              autoprefixer: {
+                flexbox: 'no-2009',
+              },
+              stage: 3,
+            }),
+            postcssNormalize(),
+          ],
+          sourceMap: shouldUseSourceMap,
+        },
+      },
+    ].filter(Boolean);
+
+    if (preProcessor) {
+      loaders.push(
+        {
+          loader: require.resolve('resolve-url-loader'),
+          options: {
+            sourceMap: shouldUseSourceMap,
+          },
+        },
+        preProcessor, // pre processor can use more option
+      );
+    }
+    return loaders;
+  };
+
+  const lessLoader = {
+    loader: require.resolve('less-loader'),
+    // not the same as react-scripts
+    options: {
+      sourceMap: shouldUseSourceMap,
+      ...lessLoaderOptions,
+    },
+  };
+
+  const defaultCSSLoaderOption = {
+    importLoaders: 2,
+    sourceMap: shouldUseSourceMap,
+  };
+
+  const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf;
+
+  // Insert less-loader as the penultimate item of loaders (before file-loader)
+  loaders.splice(
+    loaders.length - 1,
+    0,
+    {
+      test: lessRegex,
+      exclude: lessModuleRegex,
+      use: getStyleLoaders(
+        { ...defaultCSSLoaderOption, ...cssLoaderOptions, modules: false },
+        lessLoader,
+      ),
+    },
+    {
+      test: lessModuleRegex,
+      use: getStyleLoaders(
+        {
+          ...defaultCSSLoaderOption,
+          ...cssLoaderOptions,
+          modules: {
+            localIdentName: '[local]--[hash:base64:5]',
+            ...cssLoaderOptions.modules,
+          },
+        },
+        lessLoader,
+      ),
+    },
+  );
+
+  return config;
+};
